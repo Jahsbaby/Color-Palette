e65c7598150fc35b9fec144cfe72d8e6b010a6ae
Dev B <blessingjames1047@gmail.com>
2024-09-10 15:36:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,207 @@
+const header = document.querySelector("h1");
+header.style.textAlign = "center";
+header.style.marginTop = "2rem";
+header.style.fontSize = "5rem";
+
+const circle = document.querySelector("span");
+const goldButton = document.querySelector(".Gold");
+const grayButton = document.querySelector(".Gray");
+const aquaButton =  document.querySelector(".Aqua");
+const plumButton = document.querySelector(".Plum");
+const navyButton = document.querySelector(".Navy");
+const bisqueButton = document.querySelector(".Bisque");
+const khakiButton = document.querySelector(".Khaki");
+const tomatoButton = document.querySelector(".Tomato");
+const brownButton = document.querySelector(".brown");
+const yellowButton = document.querySelector(".yellow");
+const orangeButton =  document.querySelector(".orange");
+const fuchsiaButton = document.querySelector(".fuchsia");
+const crimsonButton = document.querySelector(".crimson");
+const coralButton = document.querySelector(".coral");
+const limeButton = document.querySelector(".lime");
+const dodgerblueButton = document.querySelector(".dodgerblue");
+const salmonButton = document.querySelector(".salmon");
+const goldenrodButton = document.querySelector(".goldenrod");
+const bluevioletButton = document.querySelector(".blueviolet");
+const lavenderButton = document.querySelector(".lavender");
+const turquoiseButton = document.querySelector(".turquoise");
+const yellowGreenButton = document.querySelector(".yellowgreen");
+const tanButton = document.querySelector(".tan");
+const cornflowerblueButton = document.querySelector(".cornflowerblue");
+const blackButton = document.querySelector(".black");
+const maroonButton = document.querySelector(".maroon");
+const indigoButton = document.querySelector(".indigo");
+const silverButton = document.querySelector(".silver");
+const greenButton = document.querySelector(".green");
+const violetButton = document.querySelector(".violet");
+const chocolateButton = document.querySelector(".chocolate");
+const slateblueButton = document.querySelector(".slateblue");
+const darkblueButton = document.querySelector(".darkblue");
+const cyanButton = document.querySelector(".cyan");
+const tealButton = document.querySelector(".teal");
+const orchidButton = document.querySelector(".orchid");
+const paleButton = document.querySelector(".pale");
+const lawngreenButton = document.querySelector(".lawngreen");
+const oliveButton = document.querySelector(".olive");
+const peruButton = document.querySelector(".peru");
+const firebrickButton = document.querySelector(".firebrick");
+const aquamarineButton = document.querySelector(".aquamarine");
+const burlywoodButton = document.querySelector(".burlywood");
+
+goldButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "gold";
+});
+
+grayButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "gray";
+});
+
+aquaButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "aqua";
+});
+
+plumButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "plum";
+});
+
+navyButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "navy";
+}); 
+
+bisqueButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "bisque";
+});
+
+khakiButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "khaki";
+});
+
+tomatoButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "tomato";
+});
+
+brownButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "brown";
+});
+
+yellowButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "yellow";
+});
+
+orangeButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "orange";
+});
+
+fuchsiaButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "fuchsia";
+});
+
+crimsonButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "crimson";
+});
+
+coralButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "coral";
+});
+
+limeButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "lime";
+});
+
+dodgerblueButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "dodgerblue";
+});
+
+salmonButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "salmon";
+});
+goldenrodButton.addEventListener("click", function () {
+    circle.style.backgroundColor ="goldenrod";
+});
+bluevioletButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "blueviolet";
+});
+lavenderButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "lavender";
+});
+turquoiseButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "turquoise";
+});
+yellowGreenButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "yellowgreen";
+});
+tanButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "tan";
+});
+cornflowerblueButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "cornflowerblue";
+});
+
+blackButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "black";
+});
+maroonButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "maroon";
+});
+indigoButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "indigo";
+});
+silverButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "silver";
+});
+greenButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "green";
+});
+violetButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "violet";
+});
+chocolateButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "chocolate";
+});
+slateblueButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "slateblue";
+});
+
+darkblueButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "darkblue";
+});
+
+cyanButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "cyan";
+});
+
+tealButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "teal";
+});
+
+orchidButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "orchid";
+});
+
+oliveButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "olive";
+});
+
+paleButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "pale";
+});
+
+lawngreenButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "lawngreen";
+});
+
+peruButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "peru";
+});
+
+firebrickButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "firebrick";
+});
+
+aquamarineButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "aquamarine";
+});
+
+burlywoodButton.addEventListener("click", function () {
+    circle.style.backgroundColor = "burlywood";
+});
